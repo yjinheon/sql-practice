1c6b46246c5ac4dd7d2d1fce0e9edbde0ed1d430
jinheonyoon <yjinheon@gmail.com>
2021-10-15 15:16:21 +0900

    201015 서브쿼리

--- src/programmers/programmers.sql
+++ src/programmers/programmers.sql
@@ -6,7 +6,7 @@
 -- 1. 모든 row 조회
 SELECT * from animal_ins
 
--- 2. 일부 열 조회
+-- 2. 일부 열 조회, 역순 정렬
 SELECT name, datetime
 from animal_ins
 order by animal_id desc;
@@ -17,6 +17,18 @@ from animal_ins
 where intake_condition like '%Sick%' -- like 용법 확인
 order by animal_id;
 
+
+-- 특정조건에 해당하지 '않는' 샘플 구하기 
+
+SELECT animal_id, name from animal_ins
+where intake_condition != 'Aged'
+
+
+-- 상위 n개 레코드 구하기
+SELECT name 
+from animal_ins
+order by datetime 
+limit 1
 
 -- 가장 최근 시점 구하기
 
@@ -47,6 +59,20 @@ from animal_ins
 SELECT animal_type,count(*)
 from animal_ins
 order by animal_type
+
+
+-- animal_id순으로 정렬하기
+
+select animal_id,name 
+from animal_ins
+order by animal_id
+
+
+-- 두 컬럼 기준으로 정렬하기
+
+SELECT animal_id, name, datetime
+from animal_ins
+order by name , datetime desc
 
 
 -- 동명 동물 수 찾기
---
+++ src/programmers/subquery_questions.sql
@@ -0,0 +1,3 @@
+--https://www.sqltutorial.org/sql-subquery/
+
+--https://www.w3resource.com/sql-exercises/sql-subqueries-exercises.php
